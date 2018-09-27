f61fb69582eee6563438fda0baf66af8882569c4
Kent <kli8661@bths.edu>
2018-09-27 10:18:37 -0400

    sss

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 //Kent Li - 9/25/18
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -11,32 +12,38 @@ public class Main {
         System.out.println("Pick your difficulty! 1 for 10, 2 for 100, 3 for 1000, or 4 for 10000!");
         Scanner input = new Scanner(System.in);
         int range = input.nextInt();
-        if(range == 1)
+        if (range == 1)
             range = 10;
-        if(range == 2)
+        if (range == 2)
             range = 100;
-        if(range == 3)
+        if (range == 3)
             range = 1000;
-        else if(range == 4)
+        else if (range == 4)
             range = 10000;
         Random randomNum = new Random();
         int pickedNum = randomNum.nextInt(range) + 1;
         System.out.println("Your number is between 1 and " + range + ", start guessing!");
-        int guessNum = input.nextInt();
-        int tries = 1;
-        while(guessNum != pickedNum)
+        int guessNum = range + 1;
+        int tries = 0;
+        while (guessNum != pickedNum)
         {
-            if(guessNum < pickedNum)
-            {
-                System.out.println("Your guess is less than the random number, try again!");
-                tries++;
+            try {
+                guessNum = input.nextInt();
+                if (guessNum < pickedNum) {
+                    System.out.println("Your guess is less than the random number, try again!");
+                    tries++;
+                }
+                else if (guessNum > pickedNum)
+                {
+                    System.out.println("Your guess is greater than the random number, try again!");
+                    tries++;
+                }
             }
-            else
+            catch(InputMismatchException e)
             {
-                System.out.println("Your guess is greater than the random number, try again!");
-                tries++;
+                System.out.println("Try inputting a number");
+                input.next();
             }
-            guessNum = input.nextInt();
         }
         System.out.println("Congrats! You guess the correct number in " + tries + " tries!");
         input.close();
